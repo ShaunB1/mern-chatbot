fe45dd690540a830d176206aa0be1ca14465923a
ShaunB1 <shaunbautista1@gmail.com>
2024-01-15 10:46:51 -0500

    completed mern chatbot

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div style={{width: "100%", minHeight: "20vh", maxHeight: "30vh", marginTop: 60}}>
+            <p style={{fontSize: "30px", textAlign: "center", padding: "20px"}}>
+                Built by Shaun Bautista
+            </p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer

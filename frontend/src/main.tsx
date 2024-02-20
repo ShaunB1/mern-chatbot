e52262c007b09dbc6668539f217ae005d6f8749a
ShaunB1 <shaunbautista1@gmail.com>
2024-02-20 13:22:37 -0500

    updated vercel.json

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from './context/AuthContext.tsx'
 import axios from "axios";
 import { Toaster } from "react-hot-toast";
 
-axios.defaults.baseURL = "https://mern-chatbot-hc94.vercel.app/";
+axios.defaults.baseURL = "https://mern-chatbot-hc94.vercel.app/api/v1";
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
